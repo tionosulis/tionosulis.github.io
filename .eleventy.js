c4d9c8c18100cb11a6ad6e7ff305f2f918e553e0
Sulistiono <contact.sulistiono@gmail.com>
2022-03-04 16:18:37 +0700

    Self host font

--- .eleventy.js
+++ .eleventy.js
@@ -10,8 +10,9 @@ const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("manifest.json");
   eleventyConfig.addPassthroughCopy("favicon.ico");
