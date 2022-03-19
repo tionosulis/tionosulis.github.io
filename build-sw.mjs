14a4cd6d340eebdbcca1e0fce7c46f4fa19ae3be
Sulistiono <contact.sulistiono@gmail.com>
2022-03-19 09:45:09 +0700

    Minor test

--- build-sw.mjs
+++ build-sw.mjs
@@ -4,7 +4,7 @@ import {generateSW} from 'workbox-build';
 generateSW({
   globDirectory: '_site/',
   globPatterns: [
-    '**/*.{html,css,woff2,png,svg,jpg,js,json,ico}'
+    '**/*.{html,css,woff2,png,svg,jpg,js,ico}'
   ],
   swDest: '_site/sw.js'
 });
