2e344c7ddd806047399fd251bf3dda9d83c29f73
Sulistiono <contact.sulistiono@gmail.com>
2022-12-22 01:45:55 +0700

    Add runtime caching to sw

--- build-sw.mjs
+++ build-sw.mjs
@@ -2,9 +2,54 @@
 import {generateSW} from 'workbox-build';
 
 generateSW({
+  clientsClaim: true,
   globDirectory: '_site/',
   globPatterns: [
-    '**/*.{html,css,woff2,png,svg,jpg,js,ico}'
+    '**/*.{html,svg,js,ico}'
   ],
-  swDest: '_site/sw.js'
+  swDest: '_site/sw.js',
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:css)$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'styles',
+        cacheableResponse: {
+          statuses: [0, 200] },
+        expiration: {
+          maxEntries: 3,
+        },
+      },
+    },
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:gif|jpg|png|mp4)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        cacheableResponse: {
+          statuses: [0, 200] },
+        expiration: {
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+          maxEntries: 24,
+        },
+      },
+    },
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:woff2|woff)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'font',
+        cacheableResponse: {
+          statuses: [0, 200] },
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 1,
+        },
+      },
+    },
+  ],
 });
