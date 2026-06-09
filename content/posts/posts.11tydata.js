8f57206e8ee2797511a7390eafe193efdb775473
tionosulis <contact.sulistiono@gmail.com>
2026-06-09 17:29:24 +0000

    Draft system: eleventyExcludeFromCollections + /drafts/ permalink via frontmatter draft:true

--- content/posts/posts.11tydata.js
+++ content/posts/posts.11tydata.js
@@ -1,5 +1,8 @@
 export default {
   tags: ["posts"],
   layout: "layouts/post.njk",
-  permalink: "/posts/{{ page.fileSlug }}/",
+  permalink: ({ draft, page }) => draft ? `/drafts/${page.fileSlug}/` : `/posts/${page.fileSlug}/`,
+  eleventyComputed: {
+    eleventyExcludeFromCollections: ({ draft }) => draft ? true : undefined,
+  },
 };
